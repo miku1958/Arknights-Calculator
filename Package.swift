407549debe2885d8bf4b9e11a2ed7ce466b86625
庄黛淳华 <miku1958@users.noreply.github.com>
2024-04-06 08:39:58 +0800

    get building data

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "arknights-Calculator",
+    platforms: [
+        .macOS(.v14)
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "arknights-Calculator",
+            swiftSettings: [
+                .unsafeFlags(["-enable-bare-slash-regex"])
+            ]
+        ),
+    ]
+)
